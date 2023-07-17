e75cb8ff7ef6b84433acf38de9197f611161d1dd
Sadiq <sadiq78692431@gmail.com>
2023-07-17 13:04:01 +0530

    feat - added redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </div>
   );
 }
 
